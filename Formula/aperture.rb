59e920bcc5b9e4e4eb1e0e834c58e64573d32a95
joshthomas <jthoms1@gmail.com>
2025-11-28 22:07:18 -0600

    add checksums and new builds

--- Formula/aperture.rb
+++ Formula/aperture.rb
@@ -3,22 +3,17 @@ class Aperture < Formula
   homepage "https://triptech.dev/aperture"
   version "0.1.0"
 
-  if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/triptechdev/aperture-cli/releases/download/v0.1.0/aperture-v0.1.0-aarch64-apple-darwin.tar.gz"
-    sha256 "PLACEHOLDER_AARCH64_MACOS_SHA256"
-  elsif OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/triptechdev/aperture-cli/releases/download/v0.1.0/aperture-v0.1.0-x86_64-apple-darwin.tar.gz"
-    sha256 "PLACEHOLDER_X86_64_MACOS_SHA256"
-  elsif OS.linux? && Hardware::CPU.arm?
-    url "https://github.com/triptechdev/aperture-cli/releases/download/v0.1.0/aperture-v0.1.0-aarch64-unknown-linux-gnu.tar.gz"
-    sha256 "PLACEHOLDER_AARCH64_LINUX_SHA256"
+  if OS.mac?
+    url "https://github.com/triptechdev/aperture-cli/releases/download/v0.1.0/aperture-v0.1.0-universal-apple-darwin.tar.gz"
+    sha256 "b409c2d33c8be83e02d8f72da0f840502b801c953568f979cff29b1e3649e27f"
   elsif OS.linux? && Hardware::CPU.intel?
     url "https://github.com/triptechdev/aperture-cli/releases/download/v0.1.0/aperture-v0.1.0-x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "PLACEHOLDER_X86_64_LINUX_SHA256"
+    sha256 "a66fd6de3225a850402b5a04246b301f6f6e4b13a70178e87a8a3020f64ca8fc"
   end
 
   def install
-    bin.install "aperture"
+    bin.install "aperture-universal" => "aperture" if OS.mac?
+    bin.install "aperture" if OS.linux?
 
     # Generate shell completions
     generate_completions_from_executable(bin/"aperture", "completions")
